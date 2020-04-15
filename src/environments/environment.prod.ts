d1896a51f8735beff169a8e61e884c9733825577
Yurii Krupa <yurii.krupa.dev@gmail.com>
2020-04-15 22:44:58 +0300

    [+] - deploy fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDEqrXulyb7He53kC40YiZ7kHduF4h7c60',
+    authDomain: 'dummyserver-796c0.firebaseapp.com',
+    databaseURL: 'https://dummyserver-796c0.firebaseio.com',
+    projectId: 'dummyserver-796c0',
+    storageBucket: 'dummyserver-796c0.appspot.com',
+    messagingSenderId: '351061161612',
+    appId: '1:351061161612:web:3891387829941185ae6da9',
+    measurementId: 'G-NTPVY35S1F'
+  }
 };

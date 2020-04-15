532e37b76a41738c82775583b3ff2c1fffd964c2
Yurii Krupa <yurii.krupa.dev@gmail.com>
2020-04-15 22:30:40 +0300

    [+] - added env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDEqrXulyb7He53kC40YiZ7kHduF4h7c60',
+    authDomain: 'dummyserver-796c0.firebaseapp.com',
+    databaseURL: 'https://dummyserver-796c0.firebaseio.com',
+    projectId: 'dummyserver-796c0',
+    storageBucket: 'dummyserver-796c0.appspot.com',
+    messagingSenderId: '351061161612',
+    appId: '1:351061161612:web:3891387829941185ae6da9',
+    measurementId: 'G-NTPVY35S1F'
+  }
 };
 
 /*
